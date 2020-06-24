d09b17682e4da2ade9ce2a213f6e6ea98d4a23ed
Robin Jefferson Tennison <rjefferson@sisfirst.com>
2020-06-24 06:41:12 +0530

    Initial commit

---
+++ app/src/main/java/com/sisfirst/mgsshareapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.sisfirst.mgsshareapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnShowToast.setOnClickListener {
+            Log.i("MainActivity", "Button was Clicked !")
+            Toast.makeText(this, "Button was Clicked !", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
